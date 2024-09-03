e51e0646a1e963af32cfb34caaf1643696d8f06a
MayaStelzer <maya@bipper.net>
2024-09-03 12:54:05 -0700

    starting user input

--- CPSC_353_Project_1.cpp
+++ CPSC_353_Project_1.cpp
@@ -4,13 +4,46 @@ using namespace std;
 
 int main()
 {
-    Caesar caesar = Caesar('B');
-    Vigenere vigenere = Vigenere("AB");
-    OneTimePad one_time_pad = OneTimePad("ABC");
-
-    cout << caesar.encrypt("CAT") << endl;
-    cout << vigenere.encrypt("CAT") << endl;
-    cout << one_time_pad.encrypt("CAT") << endl;
-
+    while (true) {
+        std::cout << "Choose an option:\n1. Encrypt\n2. Decrypt\n3. Exit\n";
+        int choice;
+        std::cin >> choice;
+        
+        if (choice == 3) {
+            break;
+        }
+        if (choice != 1 && choice != 2) {
+            std::cout << "Invalid choice\n";
+            continue;
+        }
+        if (choice == 1) {
+            std:: string text = get_input("Enter text to encrypt: ");
+        }
+        else {
+            std:: string text = get_input("Enter text to decrypt: ");
+        }
+        std::cout << "Choose cipher:\n1. Caesar\n2. Vigenere\n3. One-time pad\n";
+        int cipher_choice;
+        std::cin >> cipher_choice;
+        
+    }
     return 0;
+}
+
+std:: string get_input(const std::string& prompt) {
+    std:: string input;
+    std::cout << prompt;
+    std::getline(std::cin, input);
+    return input;
+}
+
+void save_to_file(std::string& filename, std::string& content) {
+    std::ofstream file(filename);
+    if (file.is_open()) {
+        file << content;
+        file.close();
+        std::cout << "Ciphertext saved to " << filename << std::endl;
+    } else {
+        std::cout << "Unable to open file for writing" << std::endl;
+    }
 }
